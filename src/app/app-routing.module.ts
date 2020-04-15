9144612af264b381cefb524b7059a9f822f90ab8
Shubham Dubey <shubhamdubey1996@gmail.com>
2020-04-15 13:13:38 +0530

    Registration Page working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+import { RegistrationComponent } from './registration/registration.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '' },
+  { 
+    path: 'register', 
+    component: RegistrationComponent,
+    data: { title: 'Register | JobBazar' },
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
