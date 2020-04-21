f7e1fbd03d49ceb1ba74b66c3b5c93996a21bc92
Shubham Dubey <shubhamdubey1996@gmail.com>
2020-04-21 23:07:06 +0530

    added redirectTo()

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,14 +10,23 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   constructor(public loginService: LoginService, public router: Router) {
-  
-   }
+
+  }
 
   ngOnInit(): void {
-    
+
   }
   public logout() {
     this.loginService.logOut();
     this.router.navigate(['login']);
+  }
+  public redirectTo() {
+    console.log("Redirect call");
+    if (this.loginService.isUserLoggedIn()) {
+      this.router.navigate(['/postedjobs']);
+    }
+    else {
+      this.router.navigate(['/login']);
+    }
   }
 }
