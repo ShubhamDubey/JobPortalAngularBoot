84e769354eeb2c424bf0573b9a9f332199a49958
Shubham Dubey <shubhamdubey1996@gmail.com>
2020-04-23 01:12:12 +0530

    added view application module

--- src/app/posted-jobs/posted-jobs.component.ts
+++ src/app/posted-jobs/posted-jobs.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Users } from '../users';
 import { JobsService } from '../jobs.service';
@@ -10,19 +11,23 @@ import { Job } from '../job';
   styleUrls: ['./posted-jobs.component.css']
 })
 export class PostedJobsComponent implements OnInit {
-  recruiter: Observable<any>= JSON.parse(sessionStorage.getItem("session"));
-  jobList:Observable<any>;
-  constructor(private jobService: JobsService) {
-    this.jobList=jobService.jobListPostedByRecruiter(this.recruiter);
+  recruiter: Observable<any> = JSON.parse(sessionStorage.getItem("session"));
+  jobList: Observable<any>;
+  constructor(private jobService: JobsService,private router:Router) {
+    this.jobList = jobService.jobListPostedByRecruiter(this.recruiter);
 
   }
 
   ngOnInit(): void {
     // this.recruiter.recruiter.jobList =
-  console.log("Recruiter");
+    console.log("Recruiter");
     console.log(this.recruiter);
     // this.jobService.postedJob(this.recruiter.recruiter).subscribe(data=>
     // console.log(data));
   }
+  public viewApplications(id) {
+    console.log(id);
+    this.router.navigate(['applications',id]);
 
+  }
 }
